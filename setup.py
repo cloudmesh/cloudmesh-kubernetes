c599dd084cf1e8251b6da7f51e8cfe6196b9f3a8
Gregor von Laszewski <laszewski@gmail.com>
2020-04-09 20:08:41 -0400

    wrong filename should be README.md

--- setup.py
+++ setup.py
@@ -15,8 +15,10 @@
 # limitations under the License.                                          #
 # ------------------------------------------------------------------------#
 
-from setuptools import find_packages, setup
 import io
+
+from setuptools import find_packages, setup
+
 
 def readfile(filename):
     with io.open(filename, encoding="utf-8") as stream:
@@ -29,7 +31,7 @@ pygments
 
 version = readfile("VERSION")[0].strip()
 
-with open('README.md') as f:
+with open('README-old.md') as f:
     long_description = f.read()
 
 
