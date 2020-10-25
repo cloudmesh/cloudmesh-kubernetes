3ca050b404b0ec531377426def83fa3c7191694f
Sunny Gandhi <sunny.gan90@gmail.com>
2020-10-24 20:12:17 -0500

    Auto deploy Kubernetes

--- cloudmesh/k3/command/k3.py
+++ cloudmesh/k3/command/k3.py
@@ -6,6 +6,7 @@ from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
 from pprint import pprint
 from cloudmesh.common.debug import VERBOSE
+import os
 
 class K3Command(PluginCommand):
 
@@ -39,9 +40,49 @@ class K3Command(PluginCommand):
             print("option a")
             m.list(path_expand(arguments.FILE))
 
+        elif arguments['deploy']:
+                if arguments['--host']:
+                        hostnames = arguments['--host']
+                        hostnames = Parameter.expand(hostnames) 
+                        self.deploy_kubernetes(hostnames) 
+
         elif arguments.list:
             print("option b")
             m.list("just calling list without parameter")
 
         Console.error("This is just a sample")
         return ""
+    
+    def deploy_kubernetes(self, hosts):
+        self.upgrade(hosts)
+        self.install_kubernetes(hosts)
+    
+    def get_url():
+        ip = os.popen("hostname -I").read()
+        realIP = ""
+        for letter in ip:
+            if(letter!= " "):
+                realIP = realIP + letter
+            else:
+                break
+        return(realIP)
+
+
+    def get_key():
+        key = os.popen("sudo cat /var/lib/rancher/k3s/server/node-token").read() 
+        return(key)
+
+    def upgrade(self, hosts):
+        command = "sudo apt-get update && sudo apt-get upgrade"
+        self.exec_on_remote_hosts(self, hosts, command)
+
+
+    def install_kubernetes(self, hosts):
+        url = get_url
+        key = get_key
+        command = 'sudo k3s agent --server https://{K3S_URL}:6443 --token {K3S_TOKEN}'.format(url, key)
+        self.exec_on_remote_hosts(self, hosts, command)
+
+    def exec_on_remote_hosts(self, hosts, command):
+         result = Host.ssh(hosts, command)
+         print(result[0]['stdout'])
