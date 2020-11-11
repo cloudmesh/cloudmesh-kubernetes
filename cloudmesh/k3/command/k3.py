b496237168288ba995cfbf338e887858d75526c1
sugandhi <sugandhi@iu.edu>
2020-11-10 21:30:19 -0500

    Test case returns hostname, IP address

--- cloudmesh/k3/command/k3.py
+++ cloudmesh/k3/command/k3.py
@@ -4,7 +4,7 @@ from cloudmesh.shell.command import PluginCommand
 from cloudmesh.k3.api.manager import Manager
 from cloudmesh.common.console import Console
 from cloudmesh.common.util import path_expand
-from pprint import pprint
+from pprint import pprintd9
 from cloudmesh.common.debug import VERBOSE
 import os
 
--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import jsonify
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    individualIP = os.popen("hostname -I").read()
+    hostname = os.popen("cat /proc/sys/kernel/hostname").read()
+    final = "The hostname is " + hostname + " .The individual IP is " + individualIP
+    return (final)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
