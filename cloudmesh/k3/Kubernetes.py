7d42c4ca06c21e99b7a841adfa051d514806f9c5
Gregor von Laszewski <laszewski@gmail.com>
2020-12-06 22:38:36 -0500

    ad Kubernetes class

---
+++ cloudmesh/k3/Kubernetes.py
@@ -0,0 +1,21 @@
+class Kuberenetes:
+
+    @staticmethod
+    def install(hosts, force=False):
+        pass
+
+    @staticmethod
+    def uninstall(hosts):
+        pass
+
+    @staticmethod
+    def status(hosts):
+        pass
+
+    @staticmethod
+    def start(hosts):
+        pass
+
+    @staticmethod
+    def stop(hosts):
+        pass
